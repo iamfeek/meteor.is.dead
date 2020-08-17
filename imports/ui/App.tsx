853cdf4e6264b4f1d19612dcf71f4878356591dd
Syafiq Hanafee <sy@fiq.me>
2020-08-17 22:18:19 +0800

    feat: navbar component
    Also removed unused components

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Hello } from './Hello';
-import { Info } from './Info';
+
+import { Navbar } from './components/Navbar';
 
 export const App = () => (
   <div>
+    <Navbar />
     <h1 className="text-xl text-blue-500 uppercase">Hello, TailwindCSS + Meteor!</h1>
-
-    <Hello />
-    <Info />
   </div>
 );
