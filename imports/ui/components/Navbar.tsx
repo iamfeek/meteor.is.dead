28d859ee5a0a40dde9c6e026ab8a09120bae806f
Syafiq Hanafee <sy@fiq.me>
2020-08-17 22:42:25 +0800

    feat(Navbar): hide More button for now

--- imports/ui/components/Navbar.tsx
+++ imports/ui/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [showMoreButton] = React.useState(false);
 
   const toggleMenuOpening = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -27,7 +28,7 @@ export const Navbar = () => {
         <nav className={`${isMenuOpen ? 'flex' : 'hidden'} flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row`}>
           <a className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="/admin">Admin</a>
 
-          <div>
+          {showMoreButton && <div>
             <button onClick={toggleMenuOpening} className="flex flex-row text-gray-900 bg-gray-200 items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
               <span>More</span>
               <svg fill="currentColor" viewBox="0 0 20 20" className={`${isMenuOpen ? "rotate-180" : "rotate-0"}} inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1`}><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
@@ -69,6 +70,7 @@ export const Navbar = () => {
                   </div>
                 </div>
               </div>
+            }
             }
           </div >
         </nav >
